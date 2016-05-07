15c05ca52e57f9067e74262c2177c3ddd0fa44a1
Michal Puncochar <puncomic@fit.cvut.cz>
2016-05-07 18:28:57 +0200

    Pridane anotace funkci a trid, osetreny IO vyjimky

--- jpgparser.py
+++ jpgparser.py
@@ -1,10 +1,20 @@
 import io
 import binascii
-from PIL import Image
 
 
 class Marker:
+    '''
+    Trida reprezentuje objekty markeru v jpeg souboru
+    Obsahuje atributy head, length, data
+    Vsechny tyto atributy jsou typu bytestring
+        head je dvojbytovy a prvni byte by mel byt 0xff
+        length je take dvojbytovy a reprezentuje delku dat + 2 v bytech
+        data jsou samotna data markeru
+    '''
     def __init__(self, head, length, data):
+        '''
+        Konstruktor prebira 3 parametry: head, length, data s tim, ze length muze byt None. V tom pripade se automaticky dopocita.
+        '''
         self.head = head
         if length == None:
             delka = len(data)+2
@@ -15,89 +25,109 @@ class Marker:
             self.length = length
         self.data = data
     def toBytes(self):
+        '''
+        Vrati bytovy retezec pouzitelny do zapisu do JPEG souboru
+        '''
         return self.head+self.length+self.data
 
         
 class JpegStructure:
+    '''
+    Trida slouzi k naparsovani JPEG souboru. Overi jeho spravnou strukturu a rozebere ho na markery a na obrazova data.
+    '''
     def __init__(self):
         self.markers = []
         self.imagestream = b''
     def addMarker(self,marker):
+        '''Prida vlastni marker. Priklad pouziti js.addMarker(Marker(head, None, data))'''
         self.markers.append(marker)
     def getMarkers(self, head):
+        '''Vrati seznam vsech markeru.'''
         r = []
         for marker in self.markers:
             if marker.head == head:
                 r.append(marker)
         return r
     def setImageStream(self,imgstream):
+        '''Nastavi obrazova data'''
         self.imagestream=imgstream
     def writeToFile(self, filename):
-        with open(filename, 'wb') as f:
-            f.write(b'\xff\xd8')
-            for marker in self.markers:
-                f.write(marker.toBytes())
-            f.write(b'\xff\xda')
-            f.write(self.imagestream)
-            f.write(b'\xff\xd9')
-        print('Zapsano do souboru '+filename)
+        '''Zapise reprezentovany JPEG soubor.'''
+        try:
+            with open(filename, 'wb') as f:
+                f.write(b'\xff\xd8')
+                for marker in self.markers:
+                    f.write(marker.toBytes())
+                f.write(b'\xff\xda')
+                f.write(self.imagestream)
+                f.write(b'\xff\xd9')
+            print('Zapsano do souboru '+filename)
+        except:
+            print('Chyba: Nepodarilo se zapsat soubor')
 
-    @staticmethod
-    def fromBytes(bts):
-        bio = io.BytesIO(bts)
-        return fromStream(bts)
+    # @staticmethod
+    # def fromBytes(bts):
+        # bio = io.BytesIO(bts)
+        # return fromStream(bts)
     
     @staticmethod
     def fromFile(fileName):
-        with open(fileName, 'rb') as f:
-            return JpegStructure.fromStream(f)
+        '''Staticka metoda. Vrati vytvorenou JpegStructure ze souboru nebo None pri chybe.'''
+        try:
+            with open(fileName, 'rb') as f:
+                return JpegStructure.fromStream(f)
+        except:
+            print('Chyba: Nepodarilo se nacist soubor')
+            return None
     
     @staticmethod
     def fromStream(stream):
+        '''Staticka metoda. Vrati vytvorenou JpegStructure ze streamu nebo None pri chybe. Priklady streamu: bytestream, filestream.'''
         js = JpegStructure()
         if (stream.read(2) != b'\xff\xd8'):
             print('Chyba: nejedna se o JPEG soubor')
             return None
 
-        while True: #jednotlive markery
+        #cyklus pres jednotlive markery
+        while True:
             bts = stream.read(2)
             if (bts == b''):
-                print('Chyba: predcasny EOF')
-                exit()
+                print('Chyba: predcasny konec souboru (EOF)')
+                return None
             # print('Marker ' + binascii.hexlify(bts).decode('ascii') + ' @ ' + str(stream.tell())+' = '+hex(stream.tell()))
             if (bts[0] != 0xff):
-                print('Chyba: Zacatek markeru se nerovna FF, ale ' + hex(bts[0]))
-                exit()
+                print('Chyba: Zacatek markeru se nerovna FF, ale ' + hex(bts[0]) + '. Pravdepodobne je chybna delka predchoziho markeru.')
+                return None
             if (bts == b'\xff\xd9'):
-                print('Chyba: Predcasny marker EOI: FF D9')
-                exit()
-            if (bts == b'\xff\xda'):
+                print('Chyba: Predcasny marker EOI (FF D9)')
+                return None
+            if (bts == b'\xff\xda'): # zacatek obrazovych dat, konec markeru
                 # print('ffda = SOS')
                 break
             bdelka = stream.read(2)
             delka = 256 * bdelka[0] + bdelka[1] - 2
             if (delka <= 0):
                 print('Chyba: zaporna delka '+delka+' = '+binascii.hexlify(bdelka).decode('ascii'))
-                exit()
-            blok = stream.read(delka)
-            marker = Marker(bts, bdelka, blok)
-            js.addMarker(marker)
+                return None
+            data = stream.read(delka)
+            js.addMarker(Marker(bts, bdelka, data))
             
+        # nacteni obrazovych dat byt po bytu
         imagestream = bytearray()
         while True:
             byte = stream.read(1)
             imagestream.append(byte[0])
             if (byte == b''):
-                print('Chyba: predcasny EOF')
-                exit()
+                print('Chyba: predcasny konec souboru (EOF)')
+                return None
             if (byte == b'\xff'):
                 tmp = stream.read(1)
                 imagestream.append(tmp[0])
-                if (tmp == b'\xd9'):
+                if (tmp == b'\xd9'): # FF D9 znaci EOI = konec streamu
                     # print('ffd9 = EOI @ ' + str(stream.tell())+' = '+hex(stream.tell()))
                     break
         
-        imagestream.pop()
+        imagestream.pop() # odstrani znacku EOI
         imagestream.pop()
         js.setImageStream(imagestream)
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import io
 import jpgparser
+from PIL import Image
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Stereoskopicke obrazky: skript umi nacist JPEG soubor a jeho levou polovinu ulozit do jineho souboru spolu s nestandartnimi metadaty. Zroven tato metadata umi i cist.')
@@ -8,15 +9,20 @@ parser.add_argument('soubor', help='nazev souboru s JPEG obrazkem')
 args = parser.parse_args()
 
 
-if args.akce == 'crop':        
+if args.akce == 'crop':
+    # vlastni test, jestli se jedna o JPEG soubor
+    if jpgparser.JpegStructure.fromFile(args.soubor) == None:
+        exit()
+    # seriznuti obrazku na polovinu a ulozeni do bytestreamu
     img = Image.open(args.soubor)
     imgCropped = img.crop((0,0,img.width//2, img.height))
-    f = io.BytesIO()
-    imgCropped.save(f, 'jpeg')
-    f.seek(0)
-
-    js = jpgparser.JpegStructure.fromStream(f)
-
+    bio = io.BytesIO()
+    imgCropped.save(bio, 'jpeg')
+    bio.seek(0)
+    
+    js = jpgparser.JpegStructure.fromStream(bio)
+    if js == None:
+        exit()
     zprava = input('Zadejte tajnou zpravu: ').encode('ascii')
     js.addMarker(jpgparser.Marker(b'\xff\xef', None, zprava))
     js.writeToFile('out_'+args.soubor)
@@ -24,6 +30,8 @@ if args.akce == 'crop':
 
 if args.akce == 'getmeta':
     js = jpgparser.JpegStructure.fromFile(args.soubor)
+    if (js == None):
+        exit()
     markers = js.getMarkers(b'\xff\xef')
     if (len(markers) <= 0):
         print('Soubor neobsahuje hledana metadata')
